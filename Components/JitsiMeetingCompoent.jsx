b9eac57e4836c02e0afb8f7603d22e1700836313
ashraful-commits <ashrfulalam10000@gmail.com>
2024-03-16 02:42:19 +0600

    meet

--- Components/JitsiMeetingCompoent.jsx
+++ Components/JitsiMeetingCompoent.jsx
@@ -1,36 +1,34 @@
-import { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const JitsiMeetComponent = () => {
-
+  const [renderJitsi, setRenderJitsi] = useState(false);
 
   useEffect(() => {
-    if (window.JitsiMeetExternalAPI) {
+    const timer = setTimeout(() => {
+      setRenderJitsi(true);
+    }, 1000); // Adjust the delay time (in milliseconds) as needed
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  useEffect(() => {
+    if (renderJitsi && window.JitsiMeetExternalAPI) {
       const api = new window.JitsiMeetExternalAPI("8x8.vc", {
         roomName: "vpaas-magic-cookie-ef69e4e2d8c644b68f667efbcbe334cd/SampleAppSacredPuzzlesRespectGenuinely",
         parentNode: document.querySelector('#jaas-container'),
       });
-  
+
       return () => {
         api.dispose();
       };
-    } else {
-      // If JitsiMeetExternalAPI is not available yet, wait for it to be available
-      const interval = setInterval(() => {
-        if (window.JitsiMeetExternalAPI) {
-          clearInterval(interval);
-          const api = new window.JitsiMeetExternalAPI("8x8.vc", {
-            roomName: "vpaas-magic-cookie-ef69e4e2d8c644b68f667efbcbe334cd/SampleAppSacredPuzzlesRespectGenuinely",
-            parentNode: document.querySelector('#jaas-container'),
-          });
-          return () => {
-            api.dispose();
-          };
-        }
-      }, 1000); // Check every second if the JitsiMeetExternalAPI is available
     }
-  }, []);
+  }, [renderJitsi]);
 
-  return <div id="jaas-container" style={{ height: '100vh' }} />;
+  return (
+    <div id="jaas-container" style={{ height: '100vh' }}>
+      {!renderJitsi && <p>Loading Jitsi Meet...</p>}
+    </div>
+  );
 };
 
 export default JitsiMeetComponent;
